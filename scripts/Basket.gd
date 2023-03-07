9adb9b8c5a8779a7b347077299c269a1209ea916
melvspace <ratealt@gmail.com>
2023-03-07 20:01:22 +0300

    style: apply redesign

--- scripts/Basket.gd
+++ scripts/Basket.gd
@@ -2,9 +2,10 @@
 extends Sprite2D
 
 
+signal on_counted;
+
 @export var ring_is_over = false :
 	set(value):
-		print('changed')
 		_toggle(value);
 		ring_is_over = value;
 	
@@ -17,13 +18,15 @@ func _set(property, value) -> bool:
 
 
 func _toggle(value: bool):
-	$ring_front_sprite.visible = value;
-	for child in $ring.get_children():
-		if not child is CollisionShape2D:
-			continue;
-			
-		var collider = child as CollisionShape2D;
-		collider.call_deferred('set_disabled', !value)
+	var sprite := $ring_sprite as Sprite2D
+	sprite.z_index = 2 if value else 0;
+	
+	var sides = $ring/ring_sides
+	var counter = $ring/counter_detector
+	
+	_toggle_collision_shapes(sides, value)
+	_toggle_collision_shapes(counter, value)
+
 
 func enable_colliders_and_front_sprite():
 	ring_is_over = true;
@@ -31,3 +34,10 @@ func enable_colliders_and_front_sprite():
 func disable_colliders_and_front_sprite():
 	ring_is_over = false;
 	
+func _toggle_collision_shapes(node: Node, value: bool):
+	for child in node.get_children():
+		if not child is CollisionShape2D:
+			continue;
+			
+		var collider = child as CollisionShape2D;
+		collider.call_deferred('set_disabled', !value)
